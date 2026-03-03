ab62fefb36dac137eca8bbf1b083d22b68676dad
Mortech <nickljeffrey@gmail.com>
2026-03-02 20:45:13 -0500

    Refactor games list

--- src/App.jsx
+++ src/App.jsx
@@ -3,32 +3,34 @@ import './App.css'
 import ReverseScrabble from './games/scrabble/ReverseScrabble'
 
 function App() {
-    const [gameComponent, setGameComponent] = useState(null)
+    const [activeComponent, setActiveComponent] = useState("")
+    const navigationVisible = activeComponent === "";
 
-    if (gameComponent !== null) {
-        return (
-            <>
-                <button onClick={() => setGameComponent(null)}>
-                        back to games list
+    const gameList = (
+        <div className="pageCards">
+            <div className="card">
+                <button onClick={() => setActiveComponent("ReverseScrabble")}>
+                    open game
                 </button>
-                {gameComponent}
-            </>
-        )
-    }
+                <p>
+                    Reverse Scrabble
+                </p>
+            </div>
+        </div>
+    )
+
+    const backButton = (
+        <button onClick={() => setActiveComponent("")}>
+            back to games list
+        </button>
+    )
 
     return (
         <>
             <h1>Daily Games</h1>
-            <div className="pageCards">
-                <div className="card">
-                    <button onClick={() => setGameComponent(ReverseScrabble)}>
-                        open game
-                    </button>
-                    <p>
-                        Reverse Scrabble
-                    </p>
-                </div>
-            </div>
+            {navigationVisible && gameList}
+            {!navigationVisible && backButton}
+            <ReverseScrabble activeComponent={activeComponent}/>
             <p className="read-the-docs">
                 Source code at <a href="https://github.com/Mortech/dailygame">https://github.com/Mortech/dailygame</a>
             </p>
--- src/games/scrabble/ReverseScrabble.jsx
+++ src/games/scrabble/ReverseScrabble.jsx
@@ -1,8 +1,15 @@
-
-function ReverseScrabble() {
+import { useState } from 'react'
+function ReverseScrabble({activeComponent}) {
+    const [count, setCount] = useState(0)
+    if (activeComponent !== "ReverseScrabble") {
+        return null;
+    }
     return (
         <>
             <h1>Hello World</h1>
+            <button onClick={() => setCount((count) => count + 1)}>
+                count is {count}
+            </button>
         </>
     )
 }
